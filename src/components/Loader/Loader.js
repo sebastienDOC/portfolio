091c567e5d90555f1362fa879955327c87cc9fa6
sebastienDOC <s.dechand21@gmail.com>
2023-10-30 14:24:07 +0100

    loader

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -5,8 +5,6 @@ import { useState } from 'react'
 
 export default function Loader() {
     const [loading, setLoading] = useState(true)
-    console.log(loading)
-
     if(loading) {
         document.body.style.overflow = "hidden";
         setTimeout(() => {
@@ -19,36 +17,36 @@ export default function Loader() {
     return (
         loading && (
             <div id='loader' className='loader-ctn'>
-                <iframe src="https://lottie.host/?file=0d9d119f-1ee3-4a96-84f1-54407b038fd3/Cyedq95xmC.json"></iframe>
-                <div class="loading">
-                    <div class="loading-letter anim-1">
+                <iframe title='Page de chargement' src="https://lottie.host/?file=0d9d119f-1ee3-4a96-84f1-54407b038fd3/Cyedq95xmC.json"></iframe>
+                <div className="loading">
+                    <div className="loading-letter anim-1">
                         C
                     </div>
-                    <div class="loading-letter anim-2">
+                    <div className="loading-letter anim-2">
                         H
                     </div>
-                    <div class="loading-letter anim-3">
+                    <div className="loading-letter anim-3">
                         A
                     </div>
-                    <div class="loading-letter anim-4">
+                    <div className="loading-letter anim-4">
                         R
                     </div>
-                    <div class="loading-letter anim-5">
+                    <div className="loading-letter anim-5">
                         G
                     </div>
-                    <div class="loading-letter anim-6">
+                    <div className="loading-letter anim-6">
                         E
                     </div>
-                    <div class="loading-letter anim-7">
+                    <div className="loading-letter anim-7">
                         M
                     </div>
-                    <div class="loading-letter anim-8">
+                    <div className="loading-letter anim-8">
                         E
                     </div>
-                    <div class="loading-letter anim-9">
+                    <div className="loading-letter anim-9">
                         N
                     </div>
-                    <div class="loading-letter anim-10">
+                    <div className="loading-letter anim-10">
                         T
                     </div>
                 </div>
